1d67544aa703b3e9ea8b2cf089ab48ed0c5050c5
awmie <amritavakole@gmail.com>
2025-03-04 07:58:54 +0530

    makrdown added

--- .venv/Lib/site-packages/g4f/Provider/base_provider.py
+++
@@ -1,4 +0,0 @@
-from ..providers.base_provider import *
-from ..providers.types import Streaming
-from ..providers.response import BaseConversation, Sources, FinishReason
-from .helper import get_cookies, format_prompt
--- .venv/Lib/site-packages/g4f/gui/server/internet.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from ...tools.web_search import SearchResults, search, get_search_message
